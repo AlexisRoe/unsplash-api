0835398b9eb80825523bb96a0700571a3cc53e52
AlexisRoe <A.Roehrling@gmx.de>
2020-10-28 16:43:28 +0100

    Build delete from favorites function

--- src/components/FavoriteImage.js
+++ src/components/FavoriteImage.js
@@ -3,12 +3,30 @@ import Button from './Button';
 import './FavoriteImage.css';
 
 function FavoriteImage({ photoId }) {
+    const storageName = 'favorites';
+
+    function deleteFavorite(photoId) {
+        let contentStorage = null;
+        try {
+            contentStorage = JSON.parse(localStorage.getItem(storageName)) || [];
+        } catch (error) {
+            console.error(error);
+            contentStorage = [];
+        }
+        const newFavorites = contentStorage.filter((favorite) => favorite !== photoId);
+        localStorage.setItem(storageName, JSON.stringify(newFavorites));
+    }
+
     return (
         <div className='favoriteContainer'>
-            <Button className='favoriteBtn-small' /*onClick={() => setFavorite(photoID)}*/>
+            <Button className='favoriteBtn-small' onClick={() => deleteFavorite(photoId)}>
                 ❤️
             </Button>
-            <img className='Image-small' src={`https://source.unsplash.com/${photoId}/800x600`} alt="favoriteOne"/>
+            <img
+                className='Image-small'
+                src={`https://source.unsplash.com/${photoId}/800x600`}
+                alt='favoriteOne'
+            />
         </div>
     );
 }
