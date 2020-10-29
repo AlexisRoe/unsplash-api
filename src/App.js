da74db25e8f9bfa9eeaf56c4f16f0f60d65ef8ee
AlexisRoe <A.Roehrling@gmx.de>
2020-10-29 14:55:42 +0100

    Pass test

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import './global.css';
-import { useState } from 'react';
-import { getRandomImage } from './api/getRandomImage';
-import ImagePreview from './components/ImagePreview';
-import Button from './components/Button';
-import FavoriteImage from './components/FavoriteImage';
+import "./global.css";
+import { useState } from "react";
+import { getRandomImage } from "./api/getRandomImage";
+import ImagePreview from "./components/ImagePreview";
+import Button from "./components/Button";
+import FavoriteImage from "./components/FavoriteImage";
+import FavoriteImageList from "./components/FavoriteImageList";
 
 function App() {
-    const [randomImage, setRandomImage] = useState(null);
+  const [randomImage, setRandomImage] = useState(null);
 
-    async function handleClick() {
-        const randomImageResponse = await getRandomImage();
-        setRandomImage(randomImageResponse);
-    }
+  async function handleClick() {
+    const randomImageResponse = await getRandomImage();
+    setRandomImage(randomImageResponse);
+  }
 
-    return (
-        <main>
-            <Button className={'buttonRandom'} onClick={() => handleClick()}>
-                Get Random Image
-            </Button>
-            {randomImage && (
-                <ImagePreview
-                    src={randomImage.urls.regular}
-                    alt={randomImage.alt_description}
-                    author={randomImage.user.name}
-                    id={randomImage.id}
-                />
-            )}
-            <div>
-                <FavoriteImage photoId={'n1B2cBi141U'} />
-            </div>
-        </main>
-    );
+  return (
+    <main id="mainContainer">
+      <Button className={"buttonRandom"} onClick={() => handleClick()}>
+        Get Random Image
+      </Button>
+      {randomImage && (
+        <ImagePreview
+          src={randomImage.urls.regular}
+          alt={randomImage.alt_description}
+          author={randomImage.user.name}
+          id={randomImage.id}
+        />
+      )}
+      <FavoriteImageList
+        photoIds={["UjD08DRW24E", "k38v31SkZdo", "AW_P_NU9MI8"]}
+      />
+      ;{/* <FavoriteImage photoId={'n1B2cBi141U'} /> */}
+    </main>
+  );
 }
 
 export default App;
--- src/components/FavoriteImageList.js
+++ src/components/FavoriteImageList.js
@@ -2,20 +2,12 @@ import "./FavoriteImageList.css";
 import FavoriteImage from "./FavoriteImage";
 import ReactDOM from "react-dom";
 
-function FavoriteImageList() {
-  function ImageList({ photoIds }) {
-    const imageList = photoIds.map((id) => (
-      <FavoriteImage key={id.toString()} photoId={id} />
-    ));
+function FavoriteImageList({ photoIds }) {
+  const imageList = photoIds.map((id) => (
+    <FavoriteImage key={id.toString()} photoId={id} />
+  ));
 
-    return <div className="Favorite-Container">{imageList}</div>;
-  }
-
-  const photoIds = ["UjD08DRW24E", "k38v31SkZdo", "AW_P_NU9MI8"];
-  ReactDOM.render(
-    <ImageList photoIds={photoIds} />,
-    document.querySelector("main")
-  );
+  return <div className="Favorite-Container">{imageList}</div>;
 }
 
 export default FavoriteImageList;
